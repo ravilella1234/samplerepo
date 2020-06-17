f2c225bbd1a4c16bf9dc50378144bcc965c75607
maha <maha@gmail.com>
2020-06-17 19:30:07 +0530

    TestNG Day-1

---
+++ JuneMavenSelenium6_30PMBatch/src/main/java/com/project/JuneMavenSelenium6_30PMBatch/App.java
@@ -0,0 +1,13 @@
+package com.project.JuneMavenSelenium6_30PMBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JuneMavenSelenium6_30PMBatch/src/test/java/com/project/JuneMavenSelenium6_30PMBatch/NewTest.java
@@ -0,0 +1,14 @@
+package com.project.JuneMavenSelenium6_30PMBatch;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+	
+  @Test
+  public void f() 
+  {
+	  System.out.println("hi");
+  }
+  
+}
